a27635b504155ff1d56a9219270afac76daf1f37
Katarina Zailac <katarina.zailac@gmail.com>
2024-06-06 10:51:45 +0200

    Redesign the outlook of configuration file to facilitate multiple tenants

--- modules/config.py
+++ modules/config.py
@@ -1,28 +1,67 @@
 import configparser
 
-conf = '/etc/argo-poem-tools/argo-poem-tools.conf'
-
 
 class Config:
-    def __init__(self):
-        self.conf = conf
+    def __init__(self, file="/etc/argo-poem-tools/argo-poem-tools.conf"):
+        self.file = file
+        self.conf = self._read()
+        self.tenants = self._get_tenants()
 
-    def read(self):
+    def _check_file_exists(self):
+        conf = configparser.ConfigParser()
+        try:
+            with open(self.file) as f:
+                conf.read_file(f)
+
+        except IOError:
+            raise ConfigException(f"File {self.file} does not exist")
+
+    def _read(self):
         config = configparser.ConfigParser()
-        config.read(self.conf)
+        config.read(self.file)
         return config
 
-    def get_hostname(self):
-        config = self.read()
-        return config.get('GENERAL', 'host')
+    def _get_tenants(self):
+        tenants = list()
+        for section in self.conf.sections():
+            if section != "GENERAL":
+                tenants.append(section)
 
-    def get_token(self):
-        config = self.read()
-        return config.get('GENERAL', 'token')
+        return tenants
+
+    def _get_entries(self, entry):
+        data = dict()
+        for tenant in self.tenants:
+            try:
+                if entry == "metricprofiles":
+                    profiles_string = self.conf.get(tenant, entry)
+                    profiles = [p.strip() for p in profiles_string.split(',')]
+
+                    data.update({tenant: profiles})
+
+                else:
+                    data.update({tenant: self.conf.get(tenant, entry)})
+
+            except configparser.NoOptionError:
+                raise ConfigException(
+                    f"Missing '{entry}' entry for tenant '{tenant}'"
+                )
+
+        return data
+
+    def get_hostnames(self):
+        return self._get_entries(entry="host")
+
+    def get_tokens(self):
+        return self._get_entries(entry="token")
 
     def get_profiles(self):
-        config = self.read()
-        profiles_string = config.get('PROFILES', 'metricprofiles')
-        profiles = [p.strip() for p in profiles_string.split(',')]
+        return self._get_entries(entry="metricprofiles")
 
-        return profiles
+
+class ConfigException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return f"Configuration file error: {str(self.msg)}"
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,29 +1,55 @@
-import configparser
 import os
 import unittest
 
-from argo_poem_tools.config import Config
+from argo_poem_tools.config import Config, ConfigException
 
 mock_file_name = 'mock-file.conf'
 
 
-file_ok = '[GENERAL]\nHost = egi.tenant.com\nToken = some-token-1234\n\n' \
-          '[PROFILES]\nMetricProfiles = TEST_PROFILE1, TEST_PROFILE2'
+file_ok = """
+[tenant1]
+Host = tenant1.example.com
+Token = some-token-1234
+MetricProfiles = TEST_PROFILE1, TEST_PROFILE2
 
-file_missing_general = '[PROFILES]\nMetricProfiles = TEST_PROFILE1, ' \
-                       'TEST_PROFILE2'
+[tenant2]
+Host = tenant2.example.com
+Token = some-token-5678
+MetricProfiles = TEST_PROFILE3, TEST_PROFILE4
+"""
 
-file_missing_host = '[GENERAL]\nToken = some-token-1234\n\n' \
-                    '[PROFILES]\nMetricProfiles = TEST_PROFILE1, TEST_PROFILE2'
+file_missing_host = """
+[tenant1]
+Token = some-token-1234
+MetricProfiles = TEST_PROFILE1, TEST_PROFILE2
 
-file_missing_token = '[GENERAL]\nHost = egi.tenant.com\n\n' \
-                     '[PROFILES]\nMetricProfiles = TEST_PROFILE1, TEST_PROFILE2'
+[tenant2]
+Host = tenant2.example.com
+Token = some-token-5678
+MetricProfiles = TEST_PROFILE3, TEST_PROFILE4
+"""
 
-file_missing_profiles = '[GENERAL]\nHost = egi.tenant.com\n' \
-                        'Token = some-token-1234'
+file_missing_token = """
+[tenant1]
+Host = tenant1.example.com
+Token = some-token-1234
+MetricProfiles = TEST_PROFILE1, TEST_PROFILE2
 
-file_missing_mp = '[GENERAL]\nHost = egi.tenant.com\nToken = some-token-1234' \
-                  '\n\n[PROFILES]\nSomeProfiles = TEST_PROFILE1, TEST_PROFILE2'
+[tenant2]
+Host = tenant2.example.com
+MetricProfiles = TEST_PROFILE3, TEST_PROFILE4
+"""
+
+file_missing_profiles = """
+[tenant1]
+Host = tenant1.example.com
+Token = some-token-1234
+
+[tenant2]
+Host = tenant2.example.com
+Token = some-token-5678
+MetricProfiles = TEST_PROFILE3, TEST_PROFILE4
+"""
 
 
 class ConfigTests(unittest.TestCase):
@@ -31,109 +57,85 @@ class ConfigTests(unittest.TestCase):
         if os.path.isfile(mock_file_name):
             os.remove(mock_file_name)
 
-    def test_read(self):
+    def test_get_hostnames(self):
         with open(mock_file_name, 'w') as f:
             f.write(file_ok)
 
-        config = Config()
-        config.conf = mock_file_name
-
-        self.assertTrue(config.read())
-
-    def test_get_hostname(self):
-        with open(mock_file_name, 'w') as f:
-            f.write(file_ok)
-
-        config = Config()
-        config.conf = mock_file_name
-        self.assertEqual(config.get_hostname(), 'egi.tenant.com')
-
-    def test_get_hostname_no_section(self):
-        with open(mock_file_name, 'w') as f:
-            f.write(file_missing_general)
-
-        config = Config()
-        config.conf = mock_file_name
-        self.assertRaises(
-            configparser.NoSectionError,
-            config.get_hostname
+        config = Config(file=mock_file_name)
+        self.assertEqual(
+            config.get_hostnames(), {
+                "tenant1": "tenant1.example.com",
+                "tenant2": "tenant2.example.com"
+            }
         )
 
-    def test_get_hostname_no_option(self):
+    def test_get_hostname_no_host(self):
         with open(mock_file_name, 'w') as f:
             f.write(file_missing_host)
 
-        config = Config()
-        config.conf = mock_file_name
+        config = Config(file=mock_file_name)
+        with self.assertRaises(ConfigException) as context:
+            config.get_hostnames()
 
-        self.assertRaises(
-            configparser.NoOptionError,
-            config.get_hostname
+        self.assertEqual(
+            context.exception.__str__(),
+            "Configuration file error: Missing 'host' entry for tenant "
+            "'tenant1'"
         )
 
-    def test_get_token(self):
+    def test_get_tokens(self):
         with open(mock_file_name, 'w') as f:
             f.write(file_ok)
 
-        config = Config()
-        config.conf = mock_file_name
-        self.assertEqual(config.get_token(), 'some-token-1234')
-
-    def test_get_token_no_section(self):
-        with open(mock_file_name, 'w') as f:
-            f.write(file_missing_general)
-
-        config = Config()
-        config.conf = mock_file_name
-
-        self.assertRaises(
-            configparser.NoSectionError,
-            config.get_token
+        config = Config(file=mock_file_name)
+        self.assertEqual(
+            config.get_tokens(), {
+                "tenant1": "some-token-1234",
+                "tenant2": "some-token-5678"
+            }
         )
 
-    def test_get_token_no_option(self):
+    def test_get_token_missing(self):
         with open(mock_file_name, 'w') as f:
             f.write(file_missing_token)
 
-        config = Config()
-        config.conf = mock_file_name
+        config = Config(file=mock_file_name)
 
-        self.assertRaises(
-            configparser.NoOptionError,
-            config.get_token
+        with self.assertRaises(ConfigException) as context:
+            config.get_tokens()
+
+        self.assertEqual(
+            context.exception.__str__(),
+            "Configuration file error: Missing 'token' entry for tenant "
+            "'tenant2'"
         )
 
     def test_get_profiles(self):
         with open(mock_file_name, 'w') as f:
             f.write(file_ok)
 
-        config = Config()
-        config.conf = mock_file_name
+        config = Config(file=mock_file_name)
 
         self.assertEqual(
-            config.get_profiles(), ['TEST_PROFILE1', 'TEST_PROFILE2']
+            config.get_profiles(), {
+                "tenant1": ['TEST_PROFILE1', 'TEST_PROFILE2'],
+                "tenant2": ["TEST_PROFILE3", "TEST_PROFILE4"]
+            }
         )
 
-    def test_get_profiles_no_section(self):
+    def test_get_profiles_missing(self):
         with open(mock_file_name, 'w') as f:
             f.write(file_missing_profiles)
 
-        config = Config()
-        config.conf = mock_file_name
-        self.assertRaises(
-            configparser.NoSectionError,
-            config.get_profiles
-        )
+        config = Config(file=mock_file_name)
 
-    def test_get_profiles_no_option(self):
-        with open(mock_file_name, 'w') as f:
-            f.write(file_missing_mp)
+        with self.assertRaises(ConfigException) as context:
+            config.get_profiles()
 
-        config = Config()
-        config.conf = mock_file_name
-        self.assertRaises(
-            configparser.NoOptionError,
-            config.get_profiles
+        self.assertEqual(
+            context.exception.__str__(),
+            "Configuration file error: Missing 'metricprofiles' entry for "
+            "tenant 'tenant1'"
         )
 
 
