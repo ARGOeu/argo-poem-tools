e9d9e462ec5de684711a38d469d7c44850467774
Katarina Zailac <katarina.zailac@gmail.com>
2025-07-23 12:57:12 +0200

    Remove notion of internal metrics from POEM class

--- modules/poem.py
+++ modules/poem.py
@@ -73,7 +73,7 @@ class POEM:
 
         return f"{name}{version}"
 
-    def _build_url(self, include_internal=False):
+    def _build_url(self):
         hostname = self.hostname
         if hostname.startswith('https://'):
             hostname = hostname[8:]
@@ -84,61 +84,25 @@ class POEM:
         if hostname.endswith('/'):
             hostname = hostname[0:-1]
 
-        if include_internal:
-            repos = "repos_internal"
-
-        else:
-            repos = "repos"
+        repos = "repos"
 
         return f"https://{hostname}/api/v2/{repos}/{self._get_os()}"
 
     def _refine_list_of_profiles(self):
         return f"[{', '.join(self.profiles)}]"
 
-    def get_data(self, include_internal=False):
+    def get_data(self):
         headers = {
             'x-api-key': self.token,
             'profiles': self._refine_list_of_profiles()
         }
         response = requests.get(self._build_url(), headers=headers, timeout=180)
 
-        data_internal = None
         missing_packages_internal = list()
-        if include_internal:
-            response_internal = requests.get(
-                self._build_url(include_internal=True),
-                headers={"x-api-key": self.token},
-                timeout=180
-            )
-
-            if response_internal.status_code == 200:
-                internal_json = response_internal.json()
-                data_internal = internal_json["data"]
-                missing_packages_internal = internal_json["missing_packages"]
-
-            else:
-                msg = f"{response_internal.status_code} " \
-                      f"{response_internal.reason}"
-
-                try:
-                    msg = f"{msg}: {response_internal.json()['detail']}"
-
-                except (ValueError, TypeError, KeyError):
-                    pass
-
-                raise POEMException(msg)
 
         if response.status_code == 200:
             data_json = response.json()
             data = data_json["data"]
-            if data_internal:
-                for name, info in data_internal.items():
-                    if name in data:
-                        p = data[name]["packages"] + info["packages"]
-                        packages = dict((v["name"], v) for v in p).values()
-                        data[name]["packages"] = sorted(
-                            packages, key=lambda k: k["name"]
-                        )
 
             self.missing_packages = sorted(
                 list(set(
